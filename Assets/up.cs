050aabca33bcbf296c03b8a97be0d0fd6a7855be
Mateusz Marciniszyn <mmmarciniszyn@gmail.com>
2017-04-07 21:04:16 +0200

    make ui fueal

---
+++ Assets/up.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class up : MonoBehaviour {
+
+    public Text ui;
+    private static int kropelka = 1;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.name == "player")
+        {
+            ui.text = "Fuel: "+ kropelka +"/10.";
+            Destroy(this.gameObject);
+            kropelka++;
+        }
+    }
+}
