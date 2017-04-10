d79195a4b8dfad5605872cc6344255dfa82a6410
Mateusz Marciniszyn <mmmarciniszyn@gmail.com>
2017-04-10 22:28:26 +0200

    add endLevel prefab

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -47,6 +47,7 @@ public class LevelManager : MonoBehaviour {
 
     public static void LevelRender(int lvl)
     {
+        sCurrentLevel = lvl;
         SceneManager.LoadScene(sPathStandart + lvl);
     }
 }
