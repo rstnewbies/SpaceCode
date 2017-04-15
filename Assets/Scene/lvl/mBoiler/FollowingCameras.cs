8aed52ec824d014cf195c826c22ff681cc1013f1
mBoiler <mmmarciniszyn@gmail.com>
2017-04-15 21:34:47 +0200

    zarysy pomysłu wielkiego umysłu

---
+++ Assets/Scene/lvl/mBoiler/FollowingCameras.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowingCameras : MonoBehaviour {
+
+    public Transform followObject;
+
+    private Vector3 offset;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // LateUpdate is called after Update each frame
+    void Update()
+    {
+        transform.position = new Vector3(followObject.position.x, followObject.position.y, transform.position.z);
+    }
+}
