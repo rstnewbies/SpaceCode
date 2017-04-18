10f3e16206924d8e58321f7af68e3b591f7b7e28
Julian <kogigogi3@wp.pl>
2017-04-18 17:08:03 +0200

    level 50 %

---
+++ Assets/Scripts/BoatSwim.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoatSwim : MonoBehaviour {
+
+    //public float speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+       
+    }
+
+    void OnTriggerStay()
+    {
+        this.GetComponent<Rigidbody2D>().AddForce(new Vector2(50f, 0f));
+    }
+
+
+}
